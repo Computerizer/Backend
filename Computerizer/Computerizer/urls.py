67064be3e1d83ce049532a070ae9e34fffe116dc
Yousef Mahmoud <mryoyo133@gmail.com>
2022-10-01 13:29:00 +0300

    setting up frontend app

--- Computerizer/Computerizer/urls.py
+++ Computerizer/Computerizer/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('Blog.urls')),
-    path('auth/', include('Oauth.urls'))
+    path('auth/', include('Oauth.urls')),
+    path('', include('frontend.urls'))
 
 ]
