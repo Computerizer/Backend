de6306c407016d3188a8f7947e50811fe5c2e74a
Yousef Mahmoud <mryoyo133@gmail.com>
2022-10-10 11:10:58 +0300

    update blog

--- Computerizer/Blog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Post, Author, Comment, LikeComment, LikePost
-
-# Register your models here.
-admin.site.register(Post)
-admin.site.register(Author)
-admin.site.register(Comment)
-admin.site.register(LikeComment)
-admin.site.register(LikePost)
