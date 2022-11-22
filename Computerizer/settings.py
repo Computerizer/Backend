9e3a76e5a6f48725889336b88624b7c3a2035984
Emad Eldin Gasser <omda.g@outlook.com>
2022-11-22 21:45:32 +0300

    🐘 Configured PostgreSQL DB

--- Computerizer/settings.py
+++ Computerizer/settings.py
@@ -95,8 +95,12 @@ WSGI_APPLICATION = 'Computerizer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'db_admin',
+        'PASSWORD': 'computerizer_postgresql_admin',
+        'HOST': 'computerizer-database.cfs83usv6ckv.us-west-2.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
