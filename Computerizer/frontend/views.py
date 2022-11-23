bfade65e99d958a2ba159f7bb85b3fc29e274f10
Yousef Mahmoud <mryoyo133@gmail.com>
2022-11-23 22:07:41 +0300

    a

--- Computerizer/frontend/views.py
+++ Computerizer/frontend/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'frontend/post.html')
+    return render(request, 'frontend/index.html')
