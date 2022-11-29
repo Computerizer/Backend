d7053f73ec48d7225a940f416067ff7ced3c9500
Emad Gasser <omda.g@outlook.com>
2022-11-29 04:01:57 +0300

    Update settings.py

--- Computerizer/settings.py
+++ Computerizer/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['computerizer.herokuapp.com', '127.0.0.1', 'https://computerizer.herokuapp.com', 'computerizr.com', 'https://computerizr.com', 'http://computerizr.com']
 
