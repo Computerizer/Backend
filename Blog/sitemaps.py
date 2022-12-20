b3f4d630c7208b6f1f3fdee1e07f9cb9f9b6df77
Emad Eldin Gasser <omda.g@outlook.com>
2022-12-20 17:32:19 +0300

    update 2

--- Blog/sitemaps.py
+++ Blog/sitemaps.py
@@ -5,7 +5,7 @@ from .models import Post
 class BlogSitemap(Sitemap):
     changefreq = "weekly"
     priority = 0.8
-    protocol = 'https'
+    protocol = 'http'
 
     def items(self):
         return Post.objects.all()
