2695ad48754aa33ce6ce7e714176a4b22df28325
Emad Eldin Gasser <omda.g@outlook.com>
2022-12-23 20:03:47 +0300

    test101

--- Computerizer/settings.py
+++ Computerizer/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['computerizer.up.railway.app', 'computerizr.com', 'https://computerizr.com/', 'https://computerizr.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
