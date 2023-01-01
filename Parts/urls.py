0be0cfe30cc2d68267285a62cabf365b54b875c7
Emad Eldin Gasser <omda.g@outlook.com>
2023-01-01 21:13:08 +0300

    added manufacturer querying to all parts via url

--- Parts/urls.py
+++ Parts/urls.py
@@ -10,21 +10,9 @@ urlpatterns = [
     path('get/watercooler', List_all_watercooler, name='watercooler'),
     path('get/aircooler', List_all_aircooler, name='aircooler'),
     path('get/mobo', List_all_mobo, name='mobo'),
-    path('get/psu', List_all_psu, name=''),
-    path('get/fan', List_all_fan, name=''),
-    path('get/case', List_all_case, name=''),
-
-    path('get/cpu/<str:manufacturer>', List_specific_cpu, name='specific-cpu'),
-    path('get/gpu/<str:manufacturer>', List_specific_gpu, name='specific-gpu'),
-    path('get/ram/<str:manufacturer>', List_specific_ram, name='specific-ram'),
-    path('get/ssd/<str:manufacturer>', List_specific_ssd, name='specific-ssd'),
-    path('get/hdd/<str:manufacturer>', List_specific_hdd, name='specific-hdd'),
-    path('get/watercooler/<str:manufacturer>', List_specific_watercooler, name='specific-watercooler'),
-    path('get/aircooler/<str:manufacturer>', List_specific_aircooler, name='specific-aircooler'),
-    path('get/mobo/<str:manufacturer>', List_specific_mobo, name='specific-mobo'),
-    path('get/psu/<str:manufacturer>', List_specific_psu, name='specific-psu'),
-    path('get/fan/<str:manufacturer>', List_specific_fan, name='specific-fan'),
-    path('get/case/<str:manufacturer>', List_specific_case, name='specific-case'),
+    path('get/psu', List_all_psu, name='psu'),
+    path('get/fan', List_all_fan, name='fan'),
+    path('get/case', List_all_case, name='case'),
 
 ]
 
--- Parts/views.py
+++ Parts/views.py
@@ -15,14 +15,22 @@ import operator
 #####################################################################
 @api_view(['GET'])
 def List_all_cpu(request):
-    try:
-        name = request.query_params.get('name')
-        if name is not None:
+    name = request.query_params.get('name')
+    manufacturer = request.query_params.get('manufacturer')
+    if name is not None:
+        try:
             model = cpu.objects.get(name=name)
             serializer = cpuSerializer(model, many=False)
             return Response(serializer.data)
-    except ObjectDoesNotExist or MultipleObjectsReturned:
-        return Response('CPU not found', status=404)
+        except ObjectDoesNotExist or MultipleObjectsReturned:
+            return Response('CPU not found', status=404)
+    if manufacturer is not None:
+        try:
+            model = cpu.objects.filter(manufacturer=manufacturer)
+            serializer = cpuSerializer(model, many=True)
+            return Response(serializer.data)
+        except ObjectDoesNotExist or Exception:
+            return Response('CPU not found', status=404)
     try: 
         model = cpu.objects.all()
         serializer = cpuSerializer(model, many=True)
@@ -36,12 +44,26 @@ def List_all_cpu(request):
 def List_all_gpu(request):
     try:
         name = request.query_params.get('name')
-        if name is not None:
+    except:
+        pass
+    try:
+        manufacturer = request.query_params.get('manufacturer')
+    except:
+        pass
+    if name is not None:
+        try:
             model = gpu.objects.get(name=name)
             serializer = gpuSerializer(model, many=False)
             return Response(serializer.data)
-    except ObjectDoesNotExist or MultipleObjectsReturned:
-        return Response(f'{name} not found', status=404)
+        except:
+            return Response('GPU not found', status=404)
+    if manufacturer is not None:
+        try:
+            model = gpu.objects.filter(manufacturer=manufacturer)
+            serializer = gpuSerializer(model, many=True)
+            return Response(serializer.data)
+        except:
+            return Response('GPU not found', status=404)
     try: 
         model = gpu.objects.all()
         serializer = gpuSerializer(model, many=True)
@@ -53,14 +75,22 @@ def List_all_gpu(request):
 
 @api_view(['GET'])
 def List_all_ram(request, ):
-    try:
-        name = request.query_params.get('name')
-        if name is not None:
+    name = request.query_params.get('name')
+    manufacturer = request.query_params.get('manufacturer')
+    if name is not None:
+        try:
             model = ram.objects.get(name=name)
             serializer = ramSerializer(model, many=False)
             return Response(serializer.data)
-    except ObjectDoesNotExist or MultipleObjectsReturned:
-        return Response('RAM not found', status=404)
+        except ObjectDoesNotExist or MultipleObjectsReturned:
+            return Response('RAM not found', status=404)
+    if manufacturer is not None:
+        try:
+            model = ram.objects.filter(manufacturer=manufacturer)
+            serializer = ramSerializer(model, many=True)
+            return Response(serializer.data)
+        except ObjectDoesNotExist or Exception:
+            return Response('RAM brand not found', status=404)
     try: 
         model = ram.objects.all()
         serializer = ramSerializer(model, many=True)
@@ -72,14 +102,22 @@ def List_all_ram(request, ):
 
 @api_view(['GET'])
 def List_all_ssd(request, ):
-    try:
-        name = request.query_params.get('name')
-        if name is not None:
+    name = request.query_params.get('name')
+    manufacturer = request.query_params.get('manufacturer')
+    if name is not None:
+        try:
             model = ssd.objects.get(name=name)
             serializer = ssdSerializer(model, many=False)
             return Response(serializer.data)
-    except ObjectDoesNotExist or MultipleObjectsReturned:
-        return Response('SSD not found', status=404)
+        except ObjectDoesNotExist or MultipleObjectsReturned:
+            return Response('SSD not found', status=404)
+    if manufacturer is not None:
+        try:
+            model = ssd.objects.filter(manufacturer=manufacturer)
+            serializer = ssdSerializer(model, many=True)
+            return Response(serializer.data)
+        except ObjectDoesNotExist or Exception:
+            return Response('SSD brand not found', status=404)
     try: 
         model = ssd.objects.all()
         serializer = ssdSerializer(model, many=True)
@@ -91,14 +129,22 @@ def List_all_ssd(request, ):
 
 @api_view(['GET'])
 def List_all_hdd(request, ):
-    try:
-        name = request.query_params.get('name')
-        if name is not None:
+    name = request.query_params.get('name')
+    manufacturer = request.query_params.get('manufacturer')
+    if name is not None:
+        try:
             model = hdd.objects.get(name=name)
             serializer = hddSerializer(model, many=False)
             return Response(serializer.data)
-    except ObjectDoesNotExist or MultipleObjectsReturned:
-        return Response('HDD not found', status=404)
+        except ObjectDoesNotExist or MultipleObjectsReturned:
+            return Response('HDD not found', status=404)
+    if manufacturer is not None:
+        try:
+            model = hdd.objects.filter(manufacturer=manufacturer)
+            serializer = hddSerializer(model, many=True)
+            return Response(serializer.data)
+        except ObjectDoesNotExist or Exception:
+            return Response('HDD brand not found', status=404)
     try: 
         model = hdd.objects.all()
         serializer = hddSerializer(model, many=True)
@@ -110,14 +156,22 @@ def List_all_hdd(request, ):
 
 @api_view(['GET'])
 def List_all_watercooler(request, ):
-    try:
-        name = request.query_params.get('name')
-        if name is not None:
+    name = request.query_params.get('name')
+    manufacturer = request.query_params.get('manufacturer')
+    if name is not None:
+        try:
             model = watercooler.objects.get(name=name)
             serializer = watercoolerSerializer(model, many=False)
             return Response(serializer.data)
-    except ObjectDoesNotExist or MultipleObjectsReturned:
-        return Response('Watercooler not found', status=404)
+        except ObjectDoesNotExist or MultipleObjectsReturned:
+            return Response('Watercooler not found', status=404)
+    if manufacturer is not None:
+        try:
+            model = watercooler.objects.filter(manufacturer=manufacturer)
+            serializer = watercoolerSerializer(model, many=True)
+            return Response(serializer.data)
+        except ObjectDoesNotExist or Exception:
+            return Response('Watercooler brand not found', status=404)
     try: 
         model = watercooler.objects.all()
         serializer = watercoolerSerializer(model, many=True)
@@ -129,33 +183,49 @@ def List_all_watercooler(request, ):
 
 @api_view(['GET'])
 def List_all_aircooler(request, ):
-    try:
-        name = request.query_params.get('name')
-        if name is not None:
+    name = request.query_params.get('name')
+    manufacturer = request.query_params.get('manufacturer')
+    if name is not None:
+        try:
             model = aircooler.objects.get(name=name)
             serializer = aircoolerSerializer(model, many=False)
             return Response(serializer.data)
-    except ObjectDoesNotExist or MultipleObjectsReturned:
-        return Response('Aircooler not found', status=404)
+        except ObjectDoesNotExist or MultipleObjectsReturned:
+            return Response('Aircooler not found', status=404)
+    if manufacturer is not None:
+        try:
+            model = aircooler.objects.filter(manufacturer=manufacturer)
+            serializer = aircoolerSerializer(model, many=True)
+            return Response(serializer.data)
+        except ObjectDoesNotExist or Exception:
+            return Response('Aircooler brand not found', status=404)
     try: 
         model = aircooler.objects.all()
         serializer = aircoolerSerializer(model, many=True)
         return Response(serializer.data)
     except Exception:
-        return Response(status=404) 
+        return Response(status=404)
     
 
 
 @api_view(['GET'])
 def List_all_mobo(request, ):
-    try:
-        name = request.query_params.get('name')
-        if name is not None:
+    name = request.query_params.get('name')
+    manufacturer = request.query_params.get('manufacturer')
+    if name is not None:
+        try:
             model = motherboard.objects.get(name=name)
             serializer = moboSerializer(model, many=False)
             return Response(serializer.data)
-    except ObjectDoesNotExist or MultipleObjectsReturned:
-        return Response('Motherboard not found', status=404)
+        except ObjectDoesNotExist or MultipleObjectsReturned:
+            return Response('Mobo not found', status=404)
+    if manufacturer is not None:
+        try:
+            model = motherboard.objects.filter(manufacturer=manufacturer)
+            serializer = moboSerializer(model, many=True)
+            return Response(serializer.data)
+        except ObjectDoesNotExist or Exception:
+            return Response('Mobo brand not found', status=404)
     try: 
         model = motherboard.objects.all()
         serializer = moboSerializer(model, many=True)
@@ -166,15 +236,23 @@ def List_all_mobo(request, ):
 
 
 @api_view(['GET'])
-def List_all_psu(request, ):
-    try:
-        name = request.query_params.get('name')
-        if name is not None:
+def List_all_psu(request):
+    name = request.query_params.get('name')
+    manufacturer = request.query_params.get('manufacturer')
+    if name is not None:
+        try:
             model = psu.objects.get(name=name)
             serializer = psuSerializer(model, many=False)
             return Response(serializer.data)
-    except ObjectDoesNotExist or MultipleObjectsReturned:
-        return Response('PSU not found', status=404)
+        except ObjectDoesNotExist or MultipleObjectsReturned:
+            return Response('PSU not found', status=404)
+    if manufacturer is not None:
+        try:
+            model = psu.objects.filter(manufacturer=manufacturer)
+            serializer = psuSerializer(model, many=True)
+            return Response(serializer.data)
+        except ObjectDoesNotExist or Exception:
+            return Response('PSU brand not found', status=404)
     try: 
         model = psu.objects.all()
         serializer = psuSerializer(model, many=True)
@@ -186,14 +264,22 @@ def List_all_psu(request, ):
 
 @api_view(['GET'])
 def List_all_fan(request, ):
-    try:
-        name = request.query_params.get('name')
-        if name is not None:
+    name = request.query_params.get('name')
+    manufacturer = request.query_params.get('manufacturer')
+    if name is not None:
+        try:
             model = fan.objects.get(name=name)
             serializer = fanSerializer(model, many=False)
             return Response(serializer.data)
-    except ObjectDoesNotExist or MultipleObjectsReturned:
-        return Response('Fan not found', status=404)
+        except ObjectDoesNotExist or MultipleObjectsReturned:
+            return Response('Fan not found', status=404)
+    if manufacturer is not None:
+        try:
+            model = fan.objects.filter(manufacturer=manufacturer)
+            serializer = fanSerializer(model, many=True)
+            return Response(serializer.data)
+        except ObjectDoesNotExist or Exception:
+            return Response('Fan brand not found', status=404)
     try: 
         model = fan.objects.all()
         serializer = fanSerializer(model, many=True)
@@ -205,97 +291,28 @@ def List_all_fan(request, ):
 
 @api_view(['GET'])
 def List_all_case(request, ):
-    try:
-        name = request.query_params.get('name')
-        if name is not None:
+    name = request.query_params.get('name')
+    manufacturer = request.query_params.get('manufacturer')
+    if name is not None:
+        try:
             model = case.objects.get(name=name)
             serializer = caseSerializer(model, many=False)
             return Response(serializer.data)
-    except ObjectDoesNotExist or MultipleObjectsReturned:
-        return Response('Case not found', status=404)
+        except ObjectDoesNotExist or MultipleObjectsReturned:
+            return Response('Case not found', status=404)
+    if manufacturer is not None:
+        try:
+            model = case.objects.filter(manufacturer=manufacturer)
+            serializer = caseSerializer(model, many=True)
+            return Response(serializer.data)
+        except ObjectDoesNotExist or Exception:
+            return Response('Case brand not found', status=404)
     try: 
         model = case.objects.all()
         serializer = caseSerializer(model, many=True)
         return Response(serializer.data)
     except Exception:
         return Response(status=404)
-    
 #####################################################################
 #####################################################################
 #####################################################################
-
-
-
-#####################################################################
-#####################################################################
-#####################################################################
-@api_view(['GET'])
-def List_specific_cpu(request, manufacturer):
-    model = cpu.objects.filter(manufacturer=manufacturer.upper())
-    serializer = cpuSerializer(model, many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def List_specific_gpu(request, manufacturer):
-    model = gpu.objects.filter(manufacturer=manufacturer.upper())
-    serializer = gpuSerializer(model, many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def List_specific_ram(request, manufacturer):
-    model = ram.objects.filter(manufacturer=manufacturer.upper())
-    serializer = ramSerializer(model, many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def List_specific_ssd(request, manufacturer):
-    model = ssd.objects.filter(manufacturer=manufacturer.upper())
-    serializer = ssdSerializer(model, many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def List_specific_hdd(request, manufacturer):
-    model = hdd.objects.filter(manufacturer=manufacturer.upper())
-    serializer = hddSerializer(model, many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def List_specific_watercooler(request, manufacturer):
-    model = watercooler.objects.filter(manufacturer=manufacturer.upper())
-    serializer = watercoolerSerializer(model, many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def List_specific_aircooler(request, manufacturer):
-    model = aircooler.objects.filter(manufacturer=manufacturer.upper())
-    serializer = aircoolerSerializer(model, many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def List_specific_mobo(request, manufacturer):
-    model = motherboard.objects.filter(manufacturer=manufacturer.upper())
-    serializer = moboSerializer(model, many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def List_specific_psu(request, manufacturer):
-    model = psu.objects.filter(manufacturer=manufacturer.upper())
-    serializer = psuSerializer(model, many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def List_specific_fan(request, manufacturer):
-    model = fan.objects.filter(manufacturer=manufacturer.upper())
-    serializer = fanSerializer(model, many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def List_specific_case(request, manufacturer):
-    model = case.objects.filter(manufacturer=manufacturer.upper())
-    serializer = caseSerializer(model, many=True)
-    return Response(serializer.data)
-#####################################################################
-#####################################################################
-#####################################################################
-
-
