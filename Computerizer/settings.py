2456c4549f2c401fd4e006f18a3782952c0abd83
Emad Eldin Gasser <omda.g@outlook.com>
2023-03-21 15:37:15 +0300

    adjusted main settings

--- Computerizer/settings.py
+++ Computerizer/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
-#MAILCHIMP_API_KEY = os.environ['MAILCHIMP_API_KEY']
-#MAILCHIMP_LIST_ID = os.environ['MAILCHIMP_LIST_ID']
+MAILCHIMP_API_KEY = os.environ['MAILCHIMP_API_KEY']
+MAILCHIMP_LIST_ID = os.environ['MAILCHIMP_LIST_ID']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['']
 
 # Application definition
 
