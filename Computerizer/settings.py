afe7339b8aa41c05bd6886cdc7a72f8c12bfeb23
Emad Gasser <omda.g@outlook.com>
2023-07-13 11:13:59 +0300

    adjust

--- Computerizer/settings.py
+++ Computerizer/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'Blog',
     'Oauth',
     'TPA',
-    'frontend',    
     'storages',
     'django.contrib.sites',
     'django.contrib.sitemaps',
@@ -144,6 +143,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'Computerizer/static')
 ]
 STATIC_URL = 'FULL-STACK/Computerizer/static'
+
 # AWS_ACCESS_KEY_ID = os.environ['AWS_S3_ACCESS_KEY_ID']
 # AWS_SECRET_ACCESS_KEY = os.environ['AWS_S3_SECRET_ACCESS_KEY']
 # AWS_STORAGE_BUCKET_NAME = 'computerizer-static'
--- frontend/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.home, name='home'),
-    path("post/<str:title>", views.post, name='post'),
-    path('about', views.about, name='about'),
-    path('recents', views.recents, name='recents')
-]
