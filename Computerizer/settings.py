39267926e21cfc23a00610147809e95158bafcb4
Emad Gasser <omda.g@outlook.com>
2023-07-13 16:18:09 +0300

    started setting up sentry

--- Computerizer/settings.py
+++ Computerizer/settings.py
@@ -15,6 +15,20 @@ from sentry_sdk.integrations.django import DjangoIntegration
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#Sentry
+sentry_sdk.init(
+dsn="https://7ea21b52dba04f69bd7fe1105e553d58@o4505521897668608.ingest.sentry.io/4505521914970112",
+integrations=[DjangoIntegration()],
+
+# Set traces_sample_rate to 1.0 to capture 100%
+# of transactions for performance monitoring.
+# We recommend adjusting this value in production.
+traces_sample_rate=1.0,
+
+# If you wish to associate users to errors (assuming you are using
+# django.contrib.auth) you may enable sending PII data.
+send_default_pii=True
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
