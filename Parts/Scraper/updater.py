2dbeef0d3a2d188bc532a9e0172ce6f5b4bb3a66
Emad Gasser <omda.g@outlook.com>
2023-07-30 00:28:19 +0300

    updated scraper with more robust user agents 👨🏼‍🚀

--- Parts/Scraper/updater.py
+++ Parts/Scraper/updater.py
@@ -1,5 +1,4 @@
-from ..models import cpu, gpu, ram, motherboard, case,\
-    aircooler, watercooler, ssd, hdd, fan, case, psu
+from ..models import cpu, gpu, ram, motherboard, case, aircooler, watercooler, ssd, hdd, fan, case, psu
 from datetime import datetime
 from .main import *
 
